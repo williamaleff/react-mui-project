8d669fc358c30724d3cdbc0162d81cfa573d64cc
WilliamAlefe <totvs.william.alefe@gmail.com>
2024-07-29 23:32:30 -0300

    feat: ferramentas da listagem criada

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -1,8 +1,15 @@
+import { FerramentasDaListagem } from "../../shared/components";
 import { LayoutBaseDePagina } from "../../shared/layouts";
 
 export const Dashboard = () => {
     return (
-        <LayoutBaseDePagina titulo="Página Inicial" barraDeFerramentas={<>Barra de ferramentas</>}>
+        <LayoutBaseDePagina 
+        titulo="Página Inicial" 
+        barraDeFerramentas={(
+            <FerramentasDaListagem
+            mostrarInputBusca
+             />
+        )}>
             teste
         </LayoutBaseDePagina>
     );
---
+++ src/shared/components/ferramentas-da-listagem/FerramentasDaListagem.tsx
@@ -0,0 +1,56 @@
+import { Box, Button, Icon, Paper, TextField, useTheme } from "@mui/material";
+
+interface IFerramentasDaListagemProps {
+    textoDaBusca?: string;
+    mostrarInputBusca?: boolean;
+    aoMudarTextoDeBusca?: (novotexto: string) => void;
+    textoBotaoNovo?: string;
+    mostrarBotaoNovo?: boolean;
+    aoClicarEmNovo?: () => void;
+}
+
+export const FerramentasDaListagem: React.FC<IFerramentasDaListagemProps> = ({
+    textoDaBusca = "",
+    mostrarInputBusca = false,
+    aoMudarTextoDeBusca,
+    aoClicarEmNovo,
+    textoBotaoNovo = "Novo",
+    mostrarBotaoNovo = true,
+}) => {
+    const theme = useTheme();
+
+    return (
+        <Box 
+        height={theme.spacing(5)} 
+        marginX={1} 
+        padding={1} 
+        paddingX={2} 
+        display="flex" 
+        gap={1} 
+        alignItems="center" 
+        component={Paper}
+        >
+            {mostrarInputBusca && (
+                <TextField
+                size="small"
+                value={textoDaBusca}
+                onChange={(e) => aoMudarTextoDeBusca?.(e.target.value)} 
+                placeholder="Pesquisar"
+                />
+    
+            )}
+
+            <Box flex={1} display="flex" justifyContent="end">
+                {mostrarBotaoNovo &&(
+                    <Button
+                    color="primary"
+                    disableElevation
+                    variant="contained"
+                    onClick={aoClicarEmNovo}
+                    endIcon={<Icon>add</Icon>}
+                    >{textoBotaoNovo}</Button>
+                )}
+            </Box>
+        </Box>
+    );
+}
