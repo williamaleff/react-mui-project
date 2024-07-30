e6cc1081b33d7533c21038c181192300e54e5d7c
WilliamAlefe <totvs.william.alefe@gmail.com>
2024-07-29 22:05:34 -0300

    feat: layout base da página

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,10 @@
-import { Button } from "@mui/material";
 import { Navigate, Route, Routes } from "react-router-dom";
 import { useDrawerContext } from "../shared/contexts";
 import { useEffect } from "react";
+import { Dashboard } from "../pages";
 
 export const AppRoutes = () => {
-    const { toggleDrawerOpen, setDrawerOptions } = useDrawerContext();
+    const { setDrawerOptions } = useDrawerContext();
 
     useEffect(() => {
         setDrawerOptions([
@@ -18,7 +18,7 @@ export const AppRoutes = () => {
 
     return (
         <Routes>
-            <Route path="/pagina-inicial" element={<Button variant="contained" color="primary" onClick={toggleDrawerOpen}>Trocar tema</Button>} />
+            <Route path="/pagina-inicial" element={<Dashboard />} />
 
             <Route path="*" element={<Navigate to="/pagina-inicial" />} />
         </Routes>
