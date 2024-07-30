79b20eeb87a0fe05a5603dc7cf5bfa72d41b4bcc
WilliamAlefe <totvs.william.alefe@gmail.com>
2024-07-30 18:45:56 -0300

    feat: atributos no botoes das ferramentas de detalhes

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -6,7 +6,7 @@ export const Dashboard = () => {
         <LayoutBaseDePagina 
         titulo="Página Inicial" 
         barraDeFerramentas={(
-            <FerramentasDeDetalhe
+            <FerramentasDeDetalhe mostrarBotaoSalvarEFechar
              />
         )}>
             teste
