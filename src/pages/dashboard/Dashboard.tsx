8127a78c8b2a717a1a142728db037583431776cc
WilliamAlefe <totvs.william.alefe@gmail.com>
2024-07-30 23:21:55 -0300

    feat: component responsivo

--- src/pages/dashboard/Dashboard.tsx
+++ src/pages/dashboard/Dashboard.tsx
@@ -6,7 +6,7 @@ export const Dashboard = () => {
         <LayoutBaseDePagina 
         titulo="Página Inicial" 
         barraDeFerramentas={(
-            <FerramentasDeDetalhe mostrarBotaoSalvarEFecharCarregando
+            <FerramentasDeDetalhe mostrarBotaoSalvarEFechar
              />
         )}>
             teste
